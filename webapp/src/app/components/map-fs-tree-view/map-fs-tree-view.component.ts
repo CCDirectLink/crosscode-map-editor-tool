385a7ef6945a7b115af793fe28bcc8daef5b7230
ac2pic <ac2pic@gmail.com>
2020-04-08 14:09:40 -0500

    Handled unsubscribing to mapFsService.fs

--- webapp/src/app/components/map-fs-tree-view/map-fs-tree-view.component.ts
+++ webapp/src/app/components/map-fs-tree-view/map-fs-tree-view.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { MapFileSystemService } from '../../shared/map-filesystem/map-filesystem.service';
 import { MatSidenav, MatTreeNestedDataSource } from '@angular/material';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-map-fs-tree-view',
     templateUrl: './map-fs-tree-view.component.html',
     styleUrls: ['./map-fs-tree-view.component.scss']
 })
-export class MapFsTreeViewComponent implements OnInit {
+export class MapFsTreeViewComponent implements OnInit, OnDestroy {
     @Input()
     sidenav!: MatSidenav;
-
+    private fsSubcription!: Subscription;
     constructor(private mapFsService: MapFileSystemService) { }
 
     ngOnInit() {
-        this.mapFsService.fs.subscribe(
+        this.fsSubcription = this.mapFsService.fs.subscribe(
             (rootFolder) => {
                 console.log(rootFolder);
             }
         );
     }
+
+    ngOnDestroy() {
+        if (this.fsSubcription) {
+            this.fsSubcription.unsubscribe();
+        }
+    }
+
 
     refresh() {
         this.mapFsService.refresh();
