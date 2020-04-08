9ab10087d542b828b50c1ca9a1e71044d68794bc
ac2pic <ac2pic@gmail.com>
2020-04-08 13:26:59 -0500

    moved mapContext.model.ts to models folder

--- webapp/src/app/services/tool-communication-api.service.ts
+++ webapp/src/app/services/tool-communication-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { MapContext } from '../components/load-map/mapContext.model';
+import { MapContext } from '../models/mapContext.model';
 
 
 @Injectable({
