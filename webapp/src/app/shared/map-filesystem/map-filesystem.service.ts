112665a1e5e6b3057d4a0d54ad18d9ffcb3a31ed
ac2pic <ac2pic@gmail.com>
2020-04-08 15:36:19 -0500

    Fixed issue where absolutePath was invalid for MapFiles

--- webapp/src/app/shared/map-filesystem/map-filesystem.service.ts
+++ webapp/src/app/shared/map-filesystem/map-filesystem.service.ts
@@ -65,7 +65,7 @@ export class MapFileSystemService {
 								value: subParent.addChildFolder({
 									type: MapFileType.FOLDER,
 									name: pathPart,
-									path: pathPart
+									path: pathPart + '/'
 								}),
 								children: {}
 							};
