ad997d818ed5416d96a51a63d888fc5e0c9ba489
ac2pic <ac2pic@gmail.com>
2020-04-12 14:53:03 -0500

    Fixed infinite loop bug that only happens in development mode

--- webapp/src/app/components/sidenav/sidenav.component.ts
+++ webapp/src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {MapLoaderService} from '../../shared/map-loader.service';
-import {CCMap} from '../../shared/phaser/tilemap/cc-map';
-import {CCMapLayer} from '../../shared/phaser/tilemap/cc-map-layer';
-import {EditorView} from '../../models/editor-view';
-import {GlobalEventsService} from '../../shared/global-events.service';
-import {MatTabChangeEvent} from '@angular/material';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MapLoaderService } from '../../shared/map-loader.service';
+import { CCMap } from '../../shared/phaser/tilemap/cc-map';
+import { CCMapLayer } from '../../shared/phaser/tilemap/cc-map-layer';
+import { EditorView } from '../../models/editor-view';
+import { GlobalEventsService } from '../../shared/global-events.service';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
 	selector: 'app-sidenav',
@@ -13,19 +13,19 @@ import {MatTabChangeEvent} from '@angular/material';
 	encapsulation: ViewEncapsulation.None
 })
 export class SidenavComponent implements OnInit {
-	
+
 	activeTab = 0;
 	selectedLayer?: CCMapLayer;
 	tilemap?: CCMap;
 	currentView?: EditorView;
 	editorViewEnum = EditorView;
-	
+	private viewChangedAgain = false;
 	constructor(
 		private mapLoader: MapLoaderService,
 		private globalEvents: GlobalEventsService
 	) {
 	}
-	
+
 	ngOnInit() {
 		this.mapLoader.selectedLayer.subscribe(layer => {
 			if (layer) {
@@ -35,13 +35,10 @@ export class SidenavComponent implements OnInit {
 		this.mapLoader.tileMap.subscribe(tilemap => {
 			this.tilemap = tilemap;
 			const currentView = this.globalEvents.currentView;
-			
-			const view = currentView.getValue();
-			currentView.next(EditorView.Layers);
-			if (view !== EditorView.Layers) {
-				currentView.next(view);
-			}
+
+			currentView.next(currentView.getValue());
 		});
+
 		this.globalEvents.currentView.subscribe(view => {
 			this.currentView = view;
 			switch (view) {
@@ -53,10 +50,11 @@ export class SidenavComponent implements OnInit {
 					break;
 			}
 		});
-		
+
 	}
-	
+
 	tabChanged(event: MatTabChangeEvent) {
+		console.log(this.activeTab, event.index, this.viewChangedAgain);
 		this.globalEvents.currentView.next(event.index === 0 ? EditorView.Layers : EditorView.Entities);
 	}
 }
