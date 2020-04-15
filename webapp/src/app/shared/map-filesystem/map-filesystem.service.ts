0a5ad8de4008805613e76f092e5145251810d748
ac2pic <ac2pic@gmail.com>
2020-04-14 19:50:33 -0500

    moved file object generator from MapFS service to utils

--- webapp/src/app/shared/map-filesystem/map-filesystem.service.ts
+++ webapp/src/app/shared/map-filesystem/map-filesystem.service.ts
@@ -38,64 +38,7 @@ export class MapFileSystemService {
 	init(maps: any) {
 		this.root.clear();
 		this.root.isRoot = true;
-		for (const map of maps) {
-
-			const mapFolder: IMapFile = {
-				type: MapFileType.FOLDER,
-				name: map.name,
-				path: map.path
-			};
-
-			const cacheSubFolders: any = {
-
-			};
-
-			const parent: MapFolder = this.root.addChildFolder(mapFolder);
-
-			parent.isRoot = true;
-			parent.setRelativePath();
-			for (const childPath of map.children) {
-				const pathParts = childPath.split('/');
-				let subParent: MapFolder = parent;
-				let subCacheFolder: any = cacheSubFolders;
-				for (let i = 0; i < pathParts.length; i++) {
-					const pathPart = pathParts[i];
-
-
-					if (i < pathParts.length - 1) {
-						if (!subCacheFolder[pathPart]) {
-							subCacheFolder[pathPart] = {
-								value: subParent.addChildFolder({
-									type: MapFileType.FOLDER,
-									name: pathPart,
-									path: pathPart + '/'
-								}),
-								children: {}
-							};
-						}
-						subParent = subCacheFolder[pathPart].value;
-						subParent.setRelativePath();
-						subCacheFolder = subCacheFolder[pathPart].children;
-					} else if (pathPart.length) {
-						let fileName: string = pathPart;
-						if (!fileName.endsWith('.json')) {
-							continue;
-						}
-
-						fileName = fileName.substring(0, fileName.length - 5);
-
-						const leafFile = subParent.addChildFile({
-							type: MapFileType.FILE,
-							name: fileName,
-							path: pathPart
-						});
-						leafFile.setRelativePath();
-					}
-
-				}
-			}
-		}
-
+		MapFileSystemUtils.generateFromMapTree(this.root, maps);
 	}
 
 
--- webapp/src/app/shared/map-filesystem/map-filesystem.utils.ts
+++ webapp/src/app/shared/map-filesystem/map-filesystem.utils.ts
@@ -1,6 +1,69 @@
-import { MapFolder, MapFile } from "./map-filesystem.model";
+import { MapFolder, MapFile, IMapFile, MapFileType } from "./map-filesystem.model";
 
 export class MapFileSystemUtils {
+    /**
+     * 
+     * @param root {MapFolder}
+     * @param maps {}
+     */
+    static generateFromMapTree(root: MapFolder, maps: any) {
+        for (const map of maps) {
+            const mapFolder: IMapFile = {
+                type: MapFileType.FOLDER,
+                name: map.name,
+                path: map.path
+            };
+
+            const cacheSubFolders: any = {
+
+            };
+
+            const parent: MapFolder = root.addChildFolder(mapFolder);
+
+            parent.isRoot = true;
+            parent.setRelativePath();
+            for (const childPath of map.children) {
+                const pathParts = childPath.split('/');
+                let subParent: MapFolder = parent;
+                let subCacheFolder: any = cacheSubFolders;
+                for (let i = 0; i < pathParts.length; i++) {
+                    const pathPart = pathParts[i];
+
+
+                    if (i < pathParts.length - 1) {
+                        if (!subCacheFolder[pathPart]) {
+                            subCacheFolder[pathPart] = {
+                                value: subParent.addChildFolder({
+                                    type: MapFileType.FOLDER,
+                                    name: pathPart,
+                                    path: pathPart + '/'
+                                }),
+                                children: {}
+                            };
+                        }
+                        subParent = subCacheFolder[pathPart].value;
+                        subParent.setRelativePath();
+                        subCacheFolder = subCacheFolder[pathPart].children;
+                    } else if (pathPart.length) {
+                        let fileName: string = pathPart;
+                        if (!fileName.endsWith('.json')) {
+                            continue;
+                        }
+
+                        fileName = fileName.substring(0, fileName.length - 5);
+
+                        const leafFile = subParent.addChildFile({
+                            type: MapFileType.FILE,
+                            name: fileName,
+                            path: pathPart
+                        });
+                        leafFile.setRelativePath();
+                    }
+
+                }
+            }
+        }
+    }
     /**
      * 
      * @param {string} path (virtual) to target folder
